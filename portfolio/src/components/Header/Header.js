ea900b8821f942ddcb0ae7b86dfb618081bb1487
AsifRaza72 <asifaligohari572@gmail.com>
2024-02-19 23:52:56 +0500

    new

---
+++ portfolio/src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import './Header.css'
+import React from 'react'
+
+const Header=()=> {
+  return (
+    <div className='Header'>
+      <div>Asif Ali</div>
+      <div>Asif Ali</div>
+      <div>Asif Ali</div>
+      <div>Asif Ali</div>
+      <div>Asif Ali</div>
+      <div>Asif Ali</div>
+      <div>Asif Ali</div>
+    </div>
+  )
+}
+
+export default Header
