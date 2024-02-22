25f587e7f57c813e5b07064cc9bc5af143075488
AsifRaza72 <asifaligohari572@gmail.com>
2024-02-23 00:49:35 +0500

    change about

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header/Header';
+import About from './components/body/About/About';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <About/>
     </div>
   );
 }
--- portfolio/src/components/body/About/About.js
+++ portfolio/src/components/body/About/About.js
@@ -1,8 +1,10 @@
 import React from "react";
+import './About.css'
 
 function About() {
   return (
-    <>
+    <div className="container">
+        <div>
       <h1>Asif Ali</h1>
       <h5>Web developer</h5>
       <p>
@@ -12,7 +14,9 @@ function About() {
         development, responsive design, and user experience optimization. I
         thrive on turning ideas into reality through clean and efficient code.
       </p>
-    </>
+      </div>
+      <div></div>
+    </div>
   );
 }
 
