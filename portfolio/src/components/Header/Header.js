5b95ca2b90d593f58c61ce595298f652b65f9bfa
AsifRaza72 <asifaligohari572@gmail.com>
2024-02-23 00:35:16 +0500

    add about

--- portfolio/src/components/Header/Header.js
+++ portfolio/src/components/Header/Header.js
@@ -1,18 +1,19 @@
-import './Header.css'
-import React from 'react'
+import "./Header.css";
+import React from "react";
 
-const Header=()=> {
+const Header = () => {
   return (
-    <div className='Header'>
-      <div>Asif Ali</div>
-      <div>Asif Ali</div>
-      <div>Asif Ali</div>
-      <div>Asif Ali</div>
-      <div>Asif Ali</div>
-      <div>Asif Ali</div>
-      <div>Asif Ali</div>
+    <div className="Header">
+      <div className="name">Asif Ali</div>
+      <div>Home</div>
+      <div>About</div>
+      <div>Resume</div>
+      <div>Skills</div>
+      <div>Projects</div>
+      <div>Services</div>
+      <div>Contact</div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
---
+++ portfolio/src/components/body/About/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function About() {
+  return (
+    <>
+      <h1>Asif Ali</h1>
+      <h5>Web developer</h5>
+      <p>
+        Hello, I'm Asif Ali, a passionate web developer with a keen eye for
+        detail and a knack for creating stunning and functional websites. With
+        1 year of experience in the field, I specialize in front-end
+        development, responsive design, and user experience optimization. I
+        thrive on turning ideas into reality through clean and efficient code.
+      </p>
+    </>
+  );
+}
+
+export default About;
