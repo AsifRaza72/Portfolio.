9e2743b5be9ddc2bd56d07ffa9044d2550a19021
AsifRaza72 <asifaligohari572@gmail.com>
2024-03-09 23:51:23 +0500

    add profile picture

--- portfolio/src/components/Header/Header.js
+++ portfolio/src/components/Header/Header.js
@@ -4,14 +4,19 @@ import React from "react";
 const Header = () => {
   return (
     <div className="Header">
+      <div className="picture"></div>
       <div className="name">Asif Ali</div>
-      <div>Home</div>
-      <div>About</div>
-      <div>Resume</div>
-      <div>Skills</div>
-      <div>Projects</div>
-      <div>Services</div>
-      <div>Contact</div>
+      <div>
+      <ul>
+      <li>Home</li>
+      <li>About</li>
+      <li>Resume</li>
+      <li>Skills</li>
+      <li>Projects</li>
+      <li>Services</li>
+      <li>Contact</li>
+      </ul>
+      </div>
     </div>
   );
 };
